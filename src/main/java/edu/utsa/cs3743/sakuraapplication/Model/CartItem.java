a6ca6fe517ccf4b87dd4483ffcce19d76d091681
Christian Fielding <Christian.Fielding@my.utsa.edu>
2025-11-14 16:30:34 -0600

    updated model files with getters and setters

--- src/main/java/edu/utsa/cs3743/sakuraapplication/Model/CartItem.java
+++ src/main/java/edu/utsa/cs3743/sakuraapplication/Model/CartItem.java
@@ -1,4 +1,38 @@
 package edu.utsa.cs3743.sakuraapplication.Model;
 
 public class CartItem {
+    private MenuItem menuItem;
+    private int quantity;
+
+    public CartItem() {}
+
+    public CartItem(MenuItem menuItem, int quantity) {
+        this.menuItem = menuItem;
+        this.quantity = quantity;
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(MenuItem menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        return menuItem.getPrice() * quantity;
+    }
+
+    @Override
+    public String toString() {
+        return menuItem.getName() + " x" + quantity;
+    }
 }
--- src/main/java/edu/utsa/cs3743/sakuraapplication/Model/User.java
+++ src/main/java/edu/utsa/cs3743/sakuraapplication/Model/User.java
@@ -1,4 +1,64 @@
 package edu.utsa.cs3743.sakuraapplication.Model;
 
 public class User {
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public User() {}
+
+    public User(int userId, String firstName, String lastName, String email, String phone) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
 }
