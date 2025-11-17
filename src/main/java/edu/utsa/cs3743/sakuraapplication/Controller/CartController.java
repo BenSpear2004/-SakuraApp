513e19e237336b6297c09ac2d197047e08241aed
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2025-11-16 23:32:49 -0600

    Update CartController.java

--- src/main/java/edu/utsa/cs3743/sakuraapplication/Controller/CartController.java
+++ src/main/java/edu/utsa/cs3743/sakuraapplication/Controller/CartController.java
@@ -1,4 +1,19 @@
 package edu.utsa.cs3743.sakuraapplication.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class CartController {
+
+    @FXML
+    public void handleHomeButton(ActionEvent event){
+        // send user back to main menu
+        System.out.println("Back to main menu");
+    }
+
+    @FXML
+    public void handleCheckoutButton(ActionEvent event){
+        // send user to check out screen
+        System.out.println("Checkout");
+    }
 }
