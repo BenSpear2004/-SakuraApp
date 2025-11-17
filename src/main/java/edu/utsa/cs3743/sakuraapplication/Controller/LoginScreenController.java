f8cc7cfd218100b631b79b4cd072eeb2b91fecd5
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2025-11-16 23:25:29 -0600

    Add comments to LoginScreenController.java

--- src/main/java/edu/utsa/cs3743/sakuraapplication/Controller/LoginScreenController.java
+++ src/main/java/edu/utsa/cs3743/sakuraapplication/Controller/LoginScreenController.java
@@ -12,11 +12,15 @@ public class LoginScreenController {
 
     @FXML
     public void handleLoginButton(ActionEvent event){
+        // check if user is already in database
+        // if not throw error
+        // else take them to main menu
         System.out.println("Login");
     }
 
     @FXML
     public void handleCreateUserButton(ActionEvent event){
+        // go to create user screen
         System.out.println("Create new User");
     }
 }
