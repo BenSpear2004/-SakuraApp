b38aef7e67ca505491af982a1fc897e05ae1b353
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2025-11-17 09:25:28 -0600

    Update LoginScreenController.java

--- src/main/java/edu/utsa/cs3743/sakuraapplication/Controller/LoginScreenController.java
+++ src/main/java/edu/utsa/cs3743/sakuraapplication/Controller/LoginScreenController.java
@@ -1,9 +1,15 @@
 package edu.utsa.cs3743.sakuraapplication.Controller;
 
+import edu.utsa.cs3743.sakuraapplication.SakuraDBApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginScreenController {
     @FXML private TextField enterUser;
@@ -14,13 +20,32 @@ public class LoginScreenController {
     public void handleLoginButton(ActionEvent event){
         // check if user is already in database
         // if not throw error
-        // else take them to main menu
-        System.out.println("Login");
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(SakuraDBApplication.class.getResource("/edu/utsa/cs3743/sakuraapplication/FXML/MainMenu.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+
+            Stage stage = new Stage();
+            stage.setTitle("Main Menu");
+            stage.setScene(scene);
+            stage.show();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     public void handleCreateUserButton(ActionEvent event){
-        // go to create user screen
-        System.out.println("Create new User");
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(SakuraDBApplication.class.getResource("/edu/utsa/cs3743/sakuraapplication/FXML/CreateUser.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+
+            Stage stage = new Stage();
+            stage.setTitle("Create Account");
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
