894a36d9107634d064e1661d44e67ee129cc6068
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2025-11-16 22:54:41 -0600

    Update LoginScreenController.java

--- src/main/java/edu/utsa/cs3743/sakuraapplication/Controller/LoginScreenController.java
+++ src/main/java/edu/utsa/cs3743/sakuraapplication/Controller/LoginScreenController.java
@@ -1,4 +1,22 @@
 package edu.utsa.cs3743.sakuraapplication.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginScreenController {
+    @FXML private TextField enterUser;
+
+    @FXML private PasswordField enterPassword;
+
+    @FXML
+    public void handleLoginButton(ActionEvent event){
+        System.out.println("Login");
+    }
+
+    @FXML
+    public void handleCreateUserButton(ActionEvent event){
+        System.out.println("Create new User");
+    }
 }
