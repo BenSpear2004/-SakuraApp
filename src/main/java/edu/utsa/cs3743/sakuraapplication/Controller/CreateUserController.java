f1d4089b40701a93e10f4600600985e6cd367dbb
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2025-11-23 21:01:28 -0600

    Update CreateUserController.java

--- src/main/java/edu/utsa/cs3743/sakuraapplication/Controller/CreateUserController.java
+++ src/main/java/edu/utsa/cs3743/sakuraapplication/Controller/CreateUserController.java
@@ -4,6 +4,8 @@ import edu.utsa.cs3743.sakuraapplication.SakuraDBApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -26,11 +28,11 @@ public class CreateUserController {
     public void handleBackButton(MouseEvent event){
         try{
             FXMLLoader fxmlLoader = new FXMLLoader(SakuraDBApplication.class.getResource("/edu/utsa/cs3743/sakuraapplication/FXML/Login-Screen.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
+            Parent root = fxmlLoader.load();
 
-            Stage stage = new Stage();
+            Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
             stage.setTitle("Create Account");
-            stage.setScene(scene);
+            stage.setScene(new Scene(root));
             stage.show();
 
         } catch(IOException e){
@@ -42,14 +44,13 @@ public class CreateUserController {
     public void handleCreateUserButton(ActionEvent event){
         // handle errors
         // add user to database
-        // take user back to login page
         try{
             FXMLLoader fxmlLoader = new FXMLLoader(SakuraDBApplication.class.getResource("/edu/utsa/cs3743/sakuraapplication/FXML/Login-Screen.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
+            Parent root = fxmlLoader.load();
 
-            Stage stage = new Stage();
+            Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
             stage.setTitle("Create Account");
-            stage.setScene(scene);
+            stage.setScene(new Scene(root));
             stage.show();
 
         } catch(IOException e){
